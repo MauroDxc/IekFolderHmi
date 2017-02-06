aa81a1e8660c826181361bfb5834473b0545a908
Mauro <dxmessage@gmail.com>
2017-02-06 01:08:28 -0600

    H

--- src/FolderHmi/AppStatics.cs
+++ src/FolderHmi/AppStatics.cs
@@ -35,7 +35,7 @@ namespace FolderHmi
             "Channel1.PLC_FOLDER.GO_NPOS_BRAZO_CDR",
             "Channel1.PLC_FOLDER.GO_NPOS_BRAZO_GM",
             "Channel1.PLC_FOLDER.GO_NPOS_BRAZO_LO",
-            "Channel1.PLC_FOLDER.GO_NPOS_BRAZO_LT",
+            "Channel1.PLC_FOLDER.GO_NPOS_BRAZO_LT", 
             "Channel1.PLC_FOLDER.HOME_D121", //25
             "Channel1.PLC_FOLDER.HOME_D123",
             "Channel1.PLC_FOLDER.HSC1_STATUS",
@@ -67,75 +67,85 @@ namespace FolderHmi
             "Channel1.PLC_FOLDER.V_MIN",
             "Channel1.PLC_FOLDER.V_MIN_BAND",
             "Channel1.PLC_FOLDER.VALOR_CERO", //55
+            "",
+            "",
+            "",
+            "",
+            "", //60
+            "",
+            "",
+            "",
+            "",
+            "", //65
 
-            "Channel1.PLC_FOLDER.C_FAULT_D110",
+            "Channel1.PLC_FOLDER.C_FAULT_D110", 
             "Channel1.PLC_FOLDER.C_FAULT_D111",
             "Channel1.PLC_FOLDER.C_FAULT_D112",
             "Channel1.PLC_FOLDER.C_FAULT_D114",
-            "Channel1.PLC_FOLDER.C_FAULT_D115",
+            "Channel1.PLC_FOLDER.C_FAULT_D115", //70
             "Channel1.PLC_FOLDER.CALIBRAR_D110",
             "Channel1.PLC_FOLDER.CALIBRAR_D111",
             "Channel1.PLC_FOLDER.CALIBRAR_D112",
             "Channel1.PLC_FOLDER.CALIBRAR_D113",
-            "Channel1.PLC_FOLDER.CALIBRAR_D114",
+            "Channel1.PLC_FOLDER.CALIBRAR_D114", //75
             "Channel1.PLC_FOLDER.CALIBRAR_D115",
             "Channel1.PLC_FOLDER.D110_FAULT",
             "Channel1.PLC_FOLDER.D111_FAULT",
             "Channel1.PLC_FOLDER.D112_FAULT",
-            "Channel1.PLC_FOLDER.D113_FAULT",
+            "Channel1.PLC_FOLDER.D113_FAULT", //80
             "Channel1.PLC_FOLDER.D114_FAULT",
             "Channel1.PLC_FOLDER.D115_FAULT",
             "Channel1.PLC_FOLDER.EN_ETHERNET_D110",
             "Channel1.PLC_FOLDER.EN_ETHERNET_D111",
-            "Channel1.PLC_FOLDER.EN_ETHERNET_D112",
+            "Channel1.PLC_FOLDER.EN_ETHERNET_D112", //85
             "Channel1.PLC_FOLDER.EN_ETHERNET_D113",
             "Channel1.PLC_FOLDER.EN_ETHERNET_D114",
             "Channel1.PLC_FOLDER.EN_ETHERNET_D115",
             "Channel1.PLC_FOLDER.END_CUELLO_A",
-            "Channel1.PLC_FOLDER.END_CUELLO_B",
+            "Channel1.PLC_FOLDER.END_CUELLO_B", //90
             "Channel1.PLC_FOLDER.END_CUELLO_C",
             "Channel1.PLC_FOLDER.END_CUELLO_D",
             "Channel1.PLC_FOLDER.END_CUELLO_REG_E",
             "Channel1.PLC_FOLDER.END_CUELLO_REG_F",
-            "Channel1.PLC_FOLDER.GO_NPOS_CUELLO_A",
+            "Channel1.PLC_FOLDER.GO_NPOS_CUELLO_A", //95
             "Channel1.PLC_FOLDER.GO_NPOS_CUELLO_B",
             "Channel1.PLC_FOLDER.GO_NPOS_CUELLO_C",
             "Channel1.PLC_FOLDER.GO_NPOS_CUELLO_D",
             "Channel1.PLC_FOLDER.GO_NPOS_REG_E",
-            "Channel1.PLC_FOLDER.GO_NPOS_REG_F",
+            "Channel1.PLC_FOLDER.GO_NPOS_REG_F", //100
             "Channel1.PLC_FOLDER.HOME_D110",
             "Channel1.PLC_FOLDER.HOME_D111",
             "Channel1.PLC_FOLDER.HOME_D112",
             "Channel1.PLC_FOLDER.HOME_D113",
-            "Channel1.PLC_FOLDER.HOME_D114",
+            "Channel1.PLC_FOLDER.HOME_D114", //105
             "Channel1.PLC_FOLDER.HOME_D115",
             "Channel1.PLC_FOLDER.IN_CUELLO_REG_E",
             "Channel1.PLC_FOLDER.IN_CUELLO_REG_F",
             "Channel1.PLC_FOLDER.INI_CUELLO_A",
-            "Channel1.PLC_FOLDER.INI_CUELLO_B",
+            "Channel1.PLC_FOLDER.INI_CUELLO_B", //110
             "Channel1.PLC_FOLDER.INI_CUELLO_C",
             "Channel1.PLC_FOLDER.INI_CUELLO_D",
             "Channel1.PLC_FOLDER.IP_DRIVE_A",
             "Channel1.PLC_FOLDER.IP_DRIVE_B",
-            "Channel1.PLC_FOLDER.IP_DRIVE_C",
+            "Channel1.PLC_FOLDER.IP_DRIVE_C", //115
             "Channel1.PLC_FOLDER.IP_DRIVE_D",
             "Channel1.PLC_FOLDER.IP_DRIVE_E",
             "Channel1.PLC_FOLDER.IP_DRIVE_F",
             "Channel1.PLC_FOLDER.NPOS_A",
-            "Channel1.PLC_FOLDER.NPOS_B",
+            "Channel1.PLC_FOLDER.NPOS_B", //120
             "Channel1.PLC_FOLDER.NPOS_C",
             "Channel1.PLC_FOLDER.NPOS_D",
             "Channel1.PLC_FOLDER.NPOS_REG_E",
             "Channel1.PLC_FOLDER.NPOS_REG_F",
-            "Channel1.PLC_FOLDER.POS_A",
+            "Channel1.PLC_FOLDER.POS_A", //125
             "Channel1.PLC_FOLDER.POS_B",
             "Channel1.PLC_FOLDER.POS_C",
             "Channel1.PLC_FOLDER.POS_D",
             "Channel1.PLC_FOLDER.POS_REG_E",
-            "Channel1.PLC_FOLDER.POS_REG_F",
+            "Channel1.PLC_FOLDER.POS_REG_F", //130
             "Channel1.PLC_FOLDER.VEL_MAX_NPOS",
             "Channel1.PLC_FOLDER.VEL_MIN_NPOS",
-
+            
         };
         public static int TagCount = TagList.Length - 1;
         public static Array ItemServerHandles;
